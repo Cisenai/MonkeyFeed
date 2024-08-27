adecbc4c365247d2ce98a410ed0e4662d0b63184
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-08-27 10:29:50 -0300

    prisma seeding

---
+++ prisma/seed.js
@@ -0,0 +1,31 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const clients = require('./data/clients.json');
+const subscriptions = require('./data/subscriptions.json');
+
+async function main() {
+    for (const u of clients) {
+        await prisma.user.create({
+            data: u,
+        });
+        console.log(`Created User`);
+    }
+    for (const s of subscriptions) {
+        await prisma.subscription.create({
+            data: s,
+        });
+        console.log(`Created Subscription`);
+    }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect;
+        console.log('Seed completed');
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect;
+        process.exit(1);
+    });
