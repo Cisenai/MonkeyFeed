25690f112479b547c3af45b5df175d8738ca902a
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-11-27 22:27:33 -0300

    front: sidebar kind working

--- front-remake/src/public/scripts/modal.js
+++ front-remake/src/public/scripts/modal.js
@@ -1,8 +1,24 @@
 const body = document.querySelector('body');
 const openModalBtn = document.querySelector('.menu');
 
-openModalBtn.addEventListener('click', (event) => {
-    fetch('/menu')
-        .then((res) => res.text())
-        .then((res) => body.innerHTML += res);
+openModalBtn.addEventListener('click', async (event) => {
+    if (!document.querySelector('.sidebar')) {
+        console.log('open modal before fetch');
+        await fetch('/menu')
+            .then((res) => res.text())
+            .then((res) => body.innerHTML += res);
+    }
 });
+
+const closeSideBar = (event) => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.remove();
+    console.log('close sidebar');
+}
+
+body.addEventListener('click', (event) => {
+    if (event.target.matches('#closeSidebar')) {
+        closeSideBar(event);
+    }
+});
+
